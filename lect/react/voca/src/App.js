fddbca932a4344ffac1dec546aaad56b874eae92
Jace Kim <jacekim.theal@gmail.com>
2025-06-03 09:47:06 +0900

    S03 Component JSX

--- lect/react/voca/src/App.js
+++ lect/react/voca/src/App.js
@@ -1,37 +1,15 @@
 import "./App.css";
+import Hello from "./component/Hello";
+import Welcome from "./component/Welcome";
 
 function App() {
 
-  const name = "React";
-  const naver = {
-    name: "네이버",
-    url: "https://www.naver.com"
-  }
-
   return (
     <div className="App">
-      <h1
-        style={{
-          color: "#EE0000",
-          backgroundColor: "lightgreen",
-          padding: "20px",
-          borderRadius: "10px",
-          fontSize: "24px",
-          textAlign: "center",
-          marginTop: "20px"
-        }}>
-        Hello, {name} !!!  
-      </h1>
-      <a href={naver.url}>
-        {naver.name}
-      </a>
+      <Hello/>
+      <Welcome name="React" />
     </div>
   );
 }
 
 export default App;
-// This code defines a simple React component that displays a greeting message and a link to Naver's website with its logo. The styles are applied inline for demonstration purposes.
-// The component uses a functional component structure and returns JSX to render the content. The `name` and `naver` objects are defined within the component, showcasing how to use variables in JSX.
-// The `App` component is exported as the default export, allowing it to be imported and used in other parts of the application, such as in the `index.js` file where it is rendered into the DOM.
-
-
--- lect/s03_component_jsx.md
+++ lect/s03_component_jsx.md
@@ -27,7 +27,7 @@ function Greeting() {
 
 <br/>
 
-### 페이지
+### 기본 페이지 - App.js
 
 - src/index.js
 ```js
@@ -83,3 +83,61 @@ export default App;
 
 - localhost:3000
 ![화면](./images/s03_react_default.png)
+
+
+---
+
+### 컴포넌트 추가
+- src/App.js
+```js
+import "./App.css";
+import Hello from "./component/Hello";
+import Welcome from "./component/Welcome";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Hello/>
+      <Welcome name="React" />
+    </div>
+  );
+}
+
+export default App;
+```
+
+- src/component/Hello.js
+```js
+import World from "./World";
+
+export default function Hello() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <World />
+    </div>
+  );
+}
+
+```
+
+- src/component/Welcome.js
+```js
+export default function Welcome() {
+  return <h1>Welcome</h1>;
+}
+
+```
+
+- src/component/World.js
+```js
+export default function World() {
+  return <h1>World</h1>;
+}
+
+```
+
+- localhost:3000
+![화면](./images/s03_react_component.png)
+
