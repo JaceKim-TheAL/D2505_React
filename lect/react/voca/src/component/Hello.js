6e5e584096895bca679f5c2a5989bca653ea7090
Jace Kim <jacekim.theal@gmail.com>
2025-06-05 19:57:14 +0900

    S08 props, properties

--- lect/react/voca/src/component/Hello.js
+++ lect/react/voca/src/component/Hello.js
@@ -2,13 +2,8 @@ import { useState } from "react";
 import UserName from "./UserName";
 
 export default function Hello({init_age = 10, ...props}) {
-  // const init_age = props.init_age;
-
-  // const [name, setName] = useState("로미오");
   const [name, setName] = useState(props.name);
-  // const [age, setAge] = useState(props.age);
   const [age, setAge] = useState(props.age === undefined ? init_age : props.age);
-
   // const msg = age > 19 ? "성인입니다." : "청소년입니다.";
   
   return (
@@ -34,9 +29,6 @@ export default function Hello({init_age = 10, ...props}) {
       <button 
         onClick={() => {
           setName(name === "로미오" ? "쥴리엣" : "로미오");
-          // setAge(age + 1); // age를 1씩 증가시킵니다.
-          // setAge(props.age + 1); // props.age는 변경할 수 없으므로, 이렇게 하면 안 됩니다.
-
         }}
       >
         이름 바꾸기
@@ -44,8 +36,6 @@ export default function Hello({init_age = 10, ...props}) {
       <button 
         onClick={() => {
           setAge(age === undefined ? init_age : age + 1);
-
-          // setAge(age + 1); // age를 1씩 증가시킵니다.
           // setAge(props.age + 1); // props.age는 변경할 수 없으므로, 이렇게 하면 안 됩니다.
         }}
       >
